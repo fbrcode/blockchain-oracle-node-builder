c8eebfbf825199c6836b729d508e24320448dd01
fbrcode <fabio.bressler@gmail.com>
2022-07-07 11:04:35 -0300

    implement repo fetching

--- src/airnode-fetch-last-release.ts
+++ src/airnode-fetch-last-release.ts
@@ -1,3 +1,18 @@
 import shell from 'shelljs';
+// import { AirnodeClone, AirnodeReleaseTag } from './types';
+import * as gitAirnode from '../config/airnode-git.json';
+import * as gitAirnodeLastRelease from '../config/airnode-last-release-tag.json';
 
-shell.echo('hello world');
+const gitClone = gitAirnode.url_clone;
+const lastRelease = gitAirnodeLastRelease.tag;
+const path = 'fetches/';
+const cloneCmd = `git clone --depth 1 --branch ${lastRelease} ${gitClone} airnode-${lastRelease}`;
+
+shell.echo(
+  `Fetching airnode release ${lastRelease} into ${path}airnode-${lastRelease}`
+);
+shell.echo(`Executing ${cloneCmd}`);
+shell.cd(path);
+shell.exec(cloneCmd);
+
+// git clone --depth 1 --branch v0.6.2 git@github.com:api3dao/airnode.git
