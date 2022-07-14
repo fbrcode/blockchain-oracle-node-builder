fb6a7508a123b97ef3dc67fa3582c4800fb1f0b3
fbrcode <fabio.bressler@gmail.com>
2022-07-14 16:29:11 -0300

    integration config helper

---
+++ config/config-helper.ts
@@ -0,0 +1,16 @@
+import { IntegrationGroup, IntegrationConfig } from '../src/types';
+
+export const integrationList: IntegrationGroup = {
+  apiConfigData: [
+    {
+      integration: 'api-multi-value',
+      airnodeType: 'local',
+      network: 'localhost',
+      mnemonic: 'test test test test test test test test test test test junk',
+      providerUrl: 'http://localhost:8545',
+    },
+  ],
+};
+
+export const integrationItem: IntegrationConfig =
+  integrationList.apiConfigData[0];
